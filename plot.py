481f9d0172b5b076532bee549f88261206eb25bd
ruhi97 <59785533+ruhi97@users.noreply.github.com>
2020-12-13 21:48:04 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import plotly.express as px
+
+df = pd.read_csv("Copy+of+data+-+data.csv")
+
+fig = px.line(df, x="date", y="cases", color="country", title='Cases all around the world')
+
+fig.show()
